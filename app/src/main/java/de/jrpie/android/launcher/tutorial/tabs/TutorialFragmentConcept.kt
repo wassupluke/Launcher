89f62fa8352e6c02298a3f669f47539c947349a8
Josia Pietsch <git@jrpie.de>
2024-07-20 01:06:02 +0200

    fix: updated tutorial

--- app/src/main/java/de/jrpie/android/launcher/tutorial/tabs/TutorialFragmentConcept.kt
+++ app/src/main/java/de/jrpie/android/launcher/tutorial/tabs/TutorialFragmentConcept.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import de.jrpie.android.launcher.BuildConfig
 import de.jrpie.android.launcher.R
 import de.jrpie.android.launcher.UIObject
+import kotlinx.android.synthetic.main.tutorial_concept.*
+import kotlinx.android.synthetic.main.tutorial_concept.view.*
 
 /**
  * The [TutorialFragmentConcept] is a used as a tab in the TutorialActivity.
@@ -19,7 +22,9 @@ class TutorialFragmentConcept : Fragment(), UIObject {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.tutorial_concept, container, false)
+        val layout = inflater.inflate(R.layout.tutorial_concept, container, false)
+        layout.tutorial_concept_badge_version.text = BuildConfig.VERSION_NAME
+        return layout
     }
 
     override fun onStart(){
