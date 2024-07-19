b5aee67ec3a3869d15781097183e963e934993d9
Josia Pietsch <git@jrpie.de>
2024-07-20 00:42:30 +0200

    fix: restart SettingsActivity after changes to settings

--- app/src/main/java/de/jrpie/android/launcher/settings/launcher/SettingsFragmentLauncher.kt
+++ app/src/main/java/de/jrpie/android/launcher/settings/launcher/SettingsFragmentLauncher.kt
@@ -93,20 +93,20 @@ class SettingsFragmentLauncher : Fragment(), UIObject {
 
 
         bindSwitchToPref(settings_launcher_switch_screen_timeout, PREF_SCREEN_TIMEOUT_DISABLED, false) {
-            setWindowFlags(activity!!.window)
+            activity?.let{setWindowFlags(it.window)}
         }
         bindSwitchToPref(settings_launcher_switch_screen_full, PREF_SCREEN_FULLSCREEN, true) {
-            setWindowFlags(activity!!.window)
+            activity?.let{setWindowFlags(it.window)}
         }
         bindSwitchToPref(settings_launcher_switch_auto_launch, PREF_SEARCH_AUTO_LAUNCH, false) {}
         bindSwitchToPref(settings_launcher_switch_auto_keyboard, PREF_SEARCH_AUTO_KEYBOARD, true) {}
         bindSwitchToPref(settings_launcher_switch_enable_double, PREF_DOUBLE_ACTIONS_ENABLED, false) {
             intendedSettingsPause = true
-            activity!!.recreate()
+            activity?.recreate()
         }
         bindSwitchToPref(settings_launcher_switch_enable_edge, PREF_EDGE_ACTIONS_ENABLED, false) {
             intendedSettingsPause = true
-            activity!!.recreate()
+            activity?.recreate()
         }
 
         settings_seekbar_sensitivity.setOnSeekBarChangeListener(
